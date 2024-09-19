0c8ae41ea722f624950b2451329b2ab969f4f930
Swetha1721 <yellalaswethareddy@gmail.com>
2024-09-19 18:59:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function rollDice() {
+        const diceNumber = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("dicerolled-Number").innerText = diceNumber;
+}
+
+    document.getElementById("rollDice").addEventListener("click", rollDice);
